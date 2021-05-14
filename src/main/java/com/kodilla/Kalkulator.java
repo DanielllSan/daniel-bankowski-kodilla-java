e5251003ba7f23217d99352c33577b5aad97aefc
DanielllSan <daniellbankowski@gmail.com>
2021-05-14 16:50:01 +0200

    Kalkulator

---
+++ src/main/java/com/kodilla/Kalkulator.java
@@ -0,0 +1,23 @@
+package com.kodilla;
+
+public class Kalkulator {
+
+
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 2;
+        int suma =a+b;
+        int odejmowanie = a-b;
+        int mnozenie = a*b;
+        int dzielenie =a/b;
+
+
+            System.out.println("Wynik dodawania to : " + suma);
+            System.out.println("Wynik odejmowania to : " + odejmowanie);
+            System.out.println("Wynik mnozenia to : " + mnozenie);
+            System.out.println("Wynik dzielenia to : " + dzielenie);
+
+
+
+    }
+}
