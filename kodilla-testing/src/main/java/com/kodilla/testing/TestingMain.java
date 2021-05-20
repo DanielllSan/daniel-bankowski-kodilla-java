22ab20cc1bde0cbbd16eb29661bb60d14a693411
DanielllSan <daniellbankowski@gmail.com>
2021-05-20 16:23:27 +0200

    6.2 Testing Calculator Class

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,40 @@
 package com.kodilla.testing;
 
+
+
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
+
 public class TestingMain {
+
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+        //nowy test dla klaSY KALKULATOR
+        int a = 4;
+        int b =4;
+        int wynikAdd = a+b;
+        int wynikSubs = a-b;
+        if( wynikAdd==8){
+            System.out.println("Dziala operacja dodawania");
+        }else{
+            System.out.println("Error");
+
+        }
+       if(wynikSubs==0){
+           System.out.println("Dziala operacja odejmowania");
+       }else{
+           System.out.println("Error");
+
+       }
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,13 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    int a;
+    int b;
+
+    public void add(int a, int b) {
+
+    }
+    public void subtract(int a,int b ) {
+
+    }
+}
